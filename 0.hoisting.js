04e298410f7c3c2111f3f9d3e3b0ec2e906a1200
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-01 15:56:50 +0600

    basir recap with TDZ and hoisted

---
+++ 0.hoisting.js
@@ -0,0 +1,8 @@
+// var is hoisted, but only the declartion , not the value assignment
+console.log(test)
+var test = 123;
+console.log(test);
+
+// console.log(name);
+let name = "Shakb";
+console.log(name);
