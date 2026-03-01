e2c4ed694d53657a86b7aed5cbc8ac03229371f8
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-01 17:18:37 +0600

    Decalre default parameter

---
+++ 2.default_parameter.js
@@ -0,0 +1,9 @@
+function multuply(a = 1, b = 1) {
+  const result = a * b;
+  return result;
+}
+
+const output = multuply(5, 5);
+const output2 = multuply(10);
+console.log(output);
+console.log(output2);
